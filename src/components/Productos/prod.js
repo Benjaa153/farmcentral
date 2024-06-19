393669ceb527cdf221611a7c0f2ad6da6f3bafc1
lucasfbrito <lucasbrifer@gmail.com>
2024-06-18 21:52:50 -0300

    pruebas

--- src/components/Productos/prod.js
+++ src/components/Productos/prod.js
@@ -1,25 +1,25 @@
 {/* Productos de perfumeria */}
 import boosNight from './Imagenes/boosNight.png';
-import poloRojo from './Imagenes/poloRojo.png'
-import alliance from './Imagenes/allianceEpicee.jpg'
-import kingAzul from './Imagenes/kingAzul.png'
-import queenrosa from './Imagenes/queenrosa.png'
-import shakirap from './Imagenes/shakirap.png'
-import hersecret from './Imagenes/hersecret.jpg'
+import poloRojo from './Imagenes/poloRojo.png';
+import alliance from './Imagenes/allianceEpicee.jpg';
+import kingAzul from './Imagenes/kingAzul.png';
+import queenrosa from './Imagenes/queenrosa.png';
+import shakirap from './Imagenes/shakirap.png';
+import hersecret from './Imagenes/hersecret.jpg';
 {/* Productos de ortopedia */}
 import silla from './Imagenes/sillaRuedas.png';
 import andador from './Imagenes/andador.png';
-import muletas from './Imagenes/muletas.png'
-import baston4 from './Imagenes/bastonCuatro.png'
-import faja from './Imagenes/faja.png'
-import bota from './Imagenes/bota.png'
+import muletas from './Imagenes/muletas.png';
+import baston4 from './Imagenes/bastonCuatro.png';
+import faja from './Imagenes/faja.png';
+import bota from './Imagenes/bota.png';
 
 {/* Productos de cuidado personal */}
-import gelLimpieza from './Imagenes/gelLimpieza.png'
-import gelExfo from './Imagenes/gelExfoliante.png'
-import protecSolar from './Imagenes/protecSolar.png'
-import shampoohead from './Imagenes/shampoohead.png'
-import desodorante from './Imagenes/desodorante.png'
+import gelLimpieza from './Imagenes/gelLimpieza.png';
+import gelExfo from './Imagenes/gelExfoliante.png';
+import protecSolar from './Imagenes/protecSolar.png';
+import shampoohead from './Imagenes/shampoohead.png';
+import desodorante from './Imagenes/desodorante.png';
 
 
 export{
@@ -44,7 +44,4 @@ export{
     protecSolar,
     shampoohead,
     desodorante,
-
-
-
 }
